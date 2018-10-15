719d14b76da11e70f815349b9a8522be417e567b
George Ho <georgeho1618@gmail.com>
2018-10-14 21:32:22 -0400

    ENH: more work on factorizations

--- quantfin/portfolio-analysis/portfolio_pset_q1.py
+++ quantfin/portfolio-analysis/portfolio_pset_q1.py
@@ -15,7 +15,9 @@ from scipy.stats import stats
 # In[2]:
 
 
-df = pd.read_csv('2010_data.csv', index_col=0)
+YEAR = 2012
+
+df = pd.read_csv('{}_data.csv'.format(YEAR), index_col=0)
 securities = df.loc[:, 'Agric':'Other']
 market = df.loc[:, 'SP500']
 interest = df.loc[:, 'LIBOR']
@@ -28,13 +30,15 @@ interest = df.loc[:, 'LIBOR']
 # In[3]:
 
 
-mu = securities.mean()
+m = securities.mean()
 cov = securities.cov()
 
 
 # ## Part (b)
 # 
-# There is fairly good agreement with theory here: all the hyperbola segments appear to lie within the envelope of another, larger hyperbola (the Markowitz bullet). On most years (e.g. 2003, 2004, 2010, 2012), the hyperbolas all slope upwards, with positive mean returns. However, in 2008, most portfolios slope downwards, with negative expected returns: this doesn't come as a surprise, given the 2008 financial crisis.
+# There is fairly good agreement with theory here: all the hyperbola segments appear to lie within the envelope of another, larger hyperbola (the Markowitz bullet). On most years (e.g. 2003, 2004, 2010, 2012), the feasible curves are all segments on the upper arm of a hyperbola: in other words, most underlying securities have positive expected return, and so a convex combination of them (i.e. no short-selling) will also have positive returns. There is stupenous agreement among years with the theory here.
+# 
+# However, in 2008, almost all portfolios correspond to the lower arm of a hyperbola, which means that you couldn't make any money if you held positive quantities of any security: not a surprising result, considering the 2008 financial crisis. The same is true with 2000 and the dot-com crash.
 
 # In[4]:
 
@@ -60,7 +64,7 @@ sigma_market = market.std()
 sigma_mvp = 1 / np.linalg.inv(cov).sum()
 weights_mvp = (sigma_mvp**2 
                * np.linalg.inv(cov) @ np.ones(cov.shape[0]))
-mu_mvp = mu @ weights_mvp
+mu_mvp = m @ weights_mvp
 
 
 # In[7]:
@@ -92,42 +96,42 @@ for mu_v, sigma_v in zip(mu_list, sigma_list):
 
 plt.xlabel('$\sigma$')
 plt.ylabel('$\mu$')
-plt.title('Five Feasible Portfolio Curves for Random Pairs of Securities')
+plt.title('{} - Five Feasible Portfolio Curves for Random Pairs of Securities'.format(YEAR))
 plt.legend(choices_list);
 
 
 # ## Part (c)
 # 
-# Depending on the year, the upper half of the hyperbola (i.e. the efficient frontier) may appear very straight. 2003, 2004 and 2010 have curves so straight they look linear, whereas 2008 has negative sloping efficient frontier. This means, somewhat nonsensically, that there is a negative risk premium: again, this is not surprising considering the financial crisis of 2008 and the violations of the assumptions of Markowitz portfolio theory.
+# Depending on the year, the upper half of the hyperbola (i.e. the efficient frontier) may appear very straight. 2003, 2004 and 2010 have curves so straight they look linear, whereas 2000 and 2008 has negative sloping efficient frontier (2014 has a "regular-looking" arm of a hyperbola). This means, somewhat nonsensically, that there is a negative risk premium: again, this is not surprising considering the financial crises of 2000 and 2008 and the violations of the assumptions of Markowitz portfolio theory.
 
 # In[8]:
 
 
 # Compute and graph efficient frontier
-m_tilde = np.vstack([mu, np.ones(mu.shape)]).T
+m_tilde = np.vstack([m, np.ones(m.shape)]).T
 B = m_tilde.T @ np.linalg.inv(cov) @ m_tilde
 G = (np.linalg.inv(B) @ m_tilde.T
      @ np.linalg.inv(cov) @ m_tilde @ np.linalg.inv(B))
 a, d, _, b = G.flatten()
 
-mu_mvp = d / a
+mu_mvp = - d / a
 sigma_mvp = np.sqrt(np.linalg.det(G) / a)
 
 mus = np.linspace(mu_mvp, mu_mvp + 1)  # A bit of kludge here...
-sigmas = np.sqrt(a*(mu_v - d/a)**2 + (1/a)*np.linalg.det(G))
+sigmas = np.sqrt(a*(mu_v + d/a)**2 + (1/a)*np.linalg.det(G))
 
 plt.figure(figsize=[12, 8])
 plt.plot(sigmas, mus)
 plt.xlabel('$\sigma$')
 plt.ylabel('$\mu$')
-plt.title('Efficient Frontier');
+plt.title('{} - Efficient Frontier'.format(YEAR));
 
 
 # ## Part (d)
 # 
-# In almost all cases (e.g. 2003, 2004, 2010, 2012), increasing mus imply increasing sigmas, thanks to the agreement with theory in part (b). Furthermore, in almost all cases (the same years as above), $w_2$ is found to be a convex combination of $w_1$ and $w_3$. This means that efficient portfolios all lie in a line segment in $w$-space, in accordance with the theory.
+# In almost all cases (e.g. 2003, 2004, 2010, 2012), increasing mus imply increasing sigmas, thanks to the agreement with theory in part (b). Furthermore, in almost all cases (the same years as above), $w_2$ is found to be a convex combination of $w_1$ and $w_3$. This means that efficient portfolios all lie in a line segment in $w$-space, in accordance with the theory. There is amazing agreement with theory here, across many years.
 # 
-# 2008 is an outlier. In 2008, increasing mus do not imply increasing sigmas, but it is still the case that $w_2$ is a convex combination of $w_1$ and $w_3$. One can only assume that the financial crash ruined many of the assumptions of Markowitz theory (e.g. no arbitrage, for instance), which ruins any theoretical guarantees.
+# 2000 and 2008 are outliers, in which increasing mus do not imply increasing sigmas, but it is still the case that $w_2$ is a convex combination of $w_1$ and $w_3$. One can only assume that the financial crash ruined many of the assumptions of Markowitz theory (e.g. no arbitrage, for instance), which ruins any theoretical guarantees.
 
 # In[9]:
 
@@ -177,9 +181,9 @@ print(msg)
 
 # ## Part (e)
 # 
-# Interestingly, in many cases (e.g. 2003, 2004, 2010, 2012), the risk-free rate is _higher_ than the mean minimum variance portfolio returns (usually, this is due to the MVP having negative expected returns). This disagreement with theory cascades down: the capital market line does not make sense (it has a negative slope, and is no longer tangent to the Markowitz bullet).
+# After the correction in the portfolio notes...
 # 
-# Thankfully, there are some years in which $ R < \mu_{MVP} $: 2008, for example.
+# In many cases (e.g. 2003, 2004, 2010, 2012), the risk-free rate is higher than the mean minimum variance portfolio returns, in agreement with theory. Again, 2008 is the exception: you would've done well to just put all your money in the money market, because even the minimum-variance portfolio failed to outperform the money market!
 # 
 # Aside: I originally forgot to convert the LIBOR from three-month returns to daily returns: the first time I actually looked at the data was when I got interest rates consistently higher than mean minimum-variance returns, and was shocked at "daily interest rates" of 6%. And yet this didn't change any of my results. Lol.
 
@@ -194,24 +198,38 @@ print(msg)
 
 # ## Part (f)
 # 
-# While we can plug our variables into the formula for the capital market line, the line does not make sense (and, depending on how you think about it, doesn't even exist). If $ R < \mu_{MVP} $, then the capital market line does not have a positive slope (which is nonsensical, since you would never take on more risk to _lower_ your expected return), and it also does not lie tangent to the Markowitz bullet (instead, it intersects it, cutting through the interior of the bullet).
+# In most cases, the capital market line exists and has a positive slope (i.e. it passes the common-sense test). This holds true for most years (e.g. 2000, 2003, 2004, 2010, 2012), so this result is extremely consistent.
+# 
+# However, if $ \mu_{M} < R $, (which is sometimes the case), then the capital market line does not have a positive slope (which is nonsensical, since you would never take on more risk to _lower_ your expected return), and it also does not lie tangent to the Markowitz bullet (instead, it intersects it, cutting through the interior of the bullet). This is true for the "crisis" year of 2008, for instance. However, 2000 (with its dot-com crash) still has a positively sloping capital market line: perhaps the dot-com crash did not violate the core assumptions of Markowitz theory, but the 2008 crash did?
 
 # In[13]:
 
 
 R = interest.mean()
-mu_M = market.mean()
-sigma_M = market.std()
+m_ex = m - R
+w_M = (1 / (np.linalg.inv(cov) @ m_ex).sum()) * np.linalg.inv(cov) @ m_ex
+mu_M = m.T @ w_M
+sigma_M = np.sqrt(w_M.T @ cov @ w_M)
 
 sigmas = np.linspace(0, 1)
 mus = R + ((mu_M - R) / sigma_M) * sigmas  # Capital Market Line
 
 
+# In[14]:
+
+
+plt.figure(figsize=[12, 8])
+plt.plot(sigmas, mus)
+plt.xlabel('$\sigma$')
+plt.ylabel('$\mu$')
+plt.title('{} - Capital Market Line'.format(YEAR));
+
+
 # ## Part (g)
 # 
-# For the year 2003, the betas versus mus appear to lie fairly close to a line with positive slope. For 2008 and 2012, they lie fairly close to a line with negative slope. However, for 2004 and 2010, they appear fairly scattered. In general, I feel as if 5 data points is not enough to determine if the points obey a linear relation.
+# This is a toss in all ways: most years lie in a line of zero slope, whereas some look more scattered. In no case do they ever actually align into a positively or negatively sloping line. I have no idea why this is: a horizontal line would imply that each portfolio is fairly uncorrelated with the market, irrespective of the expected returns.
 
-# In[14]:
+# In[15]:
 
 
 def weights_to_beta(weights):
@@ -222,7 +240,7 @@ def weights_to_beta(weights):
     return beta, returns_portfolio  # Return portfolio returns for part (h)
 
 
-# In[15]:
+# In[16]:
 
 
 beta_mvp, returns_mvp = weights_to_beta(weights_mvp)
@@ -232,14 +250,14 @@ beta3, returns3 = weights_to_beta(w3)
 beta_naive, returns_naive = weights_to_beta(np.ones(48) / 48)
 
 
-# In[16]:
+# In[17]:
 
 
 fig = plt.figure(figsize=[12, 8])
 mus = [mu_M, mu_mvp, mu1, mu2, mu3, mu_naive]
 betas = [0, beta_mvp, beta1, beta2, beta3, beta_naive]
 plt.scatter(mus, betas)
-plt.title('Betas versus mean returns')
+plt.title('{} - Betas vs Expected Returns'.format(YEAR))
 plt.xlabel('$\mu$')
 plt.ylabel('beta');
 
@@ -248,7 +266,7 @@ plt.ylabel('beta');
 # 
 # Depending on the year, and your luck with the randomly chosen portfolios, these equity curves may seem very unreasonable. Oddly enough, for a few years (e.g. 2004), the naive portfolio fluctuates wildly, achieving returns as much as 250% and as low as -100%. This may be due to a data error, or could be attributable to naivety of the portfolio itself (since it usually achieves a mean return very close to 0).
 
-# In[17]:
+# In[18]:
 
 
 equity_market = (1+market).cumprod()
@@ -260,7 +278,7 @@ equity2 = (1+returns2).cumprod()
 equity3 = (1+returns3).cumprod()
 
 
-# In[18]:
+# In[19]:
 
 
 fig = plt.figure(figsize=[12, 8])
@@ -279,7 +297,7 @@ for index, label in enumerate(fig.axes[0].xaxis.get_ticklabels()):
     
 plt.xlabel('Time')
 plt.ylabel('Cumulative Returns')
-plt.title('Equity Curves')
+plt.title('{} - Equity Curves'.format(YEAR))
 plt.xticks(rotation=45)
 plt.legend();
 

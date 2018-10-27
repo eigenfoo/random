390c27864caa57daec145c6ebac0a247b9dd5896
George Ho <georgeho1618@gmail.com>
2018-10-27 16:29:43 -0400

    BLD: working on q1

--- quantfin/stochastic-calculus/stoch_calc_pset_q1.py
+++ quantfin/stochastic-calculus/stoch_calc_pset_q1.py
@@ -8,6 +8,7 @@
 
 import numpy as np
 import pandas as pd
+from itertools import product
 
 
 # In[2]:
@@ -71,6 +72,8 @@ p_tilde, q_tilde = risk_neutral_probabilities(u, d, r)
 
 
 # ## Part (c)
+# 
+# Modified in email. This question now contains code to compute $\Delta_n$ and $X_n$ given a path $\omega_n$.
 
 # In[4]:
 
@@ -90,13 +93,16 @@ path = np.random.randint(low=0, high=2, size=N)
 # In[5]:
 
 
-def one_step_back(omega_np1, X_np1, derivative_value, stock_value):
-    heads_case = np.append(omega_np1[:-1], 1)
-    tails_case = np.append(omega_np1[:-1], 0)
+def portfolio_from_path(omega_n, derivative_value, stock_value):
+    '''
+    Given a path omega_n, computes the delta_n and X_n.
+    '''
+    heads_case = np.append(omega_n, 1)
+    tails_case = np.append(omega_n, 0)
     numerator = derivative_value(heads_case) - derivative_value(tails_case)
     denominator = stock_value(heads_case) - stock_value(tails_case)
     delta_n = numerator / denominator
-    X_n = 1/(1+r) * (X_np1 - delta_n*(stock_value(omega_np1) - (1+r)*stock_value(omega_np1[:-1])))
+    X_n = derivative_value(omega_n)  # X_n = V_n almost surely
     return delta_n, X_n
 
 
@@ -135,9 +141,19 @@ def european_put(paths, strike_price):
     return out
 
 
+# In[9]:
+
+
+# Check that the functions work
+path = np.random.binomial(n=1, p=p_tilde, size=[100])
+_ = exotic_derivative(path)
+_ = european_call(path, 1)
+_ = european_put(path, 1)
+
+
 # ## Part (e)
 
-# In[9]:
+# In[10]:
 
 
 probabilities = [p_tilde, p_1, p_2]
@@ -145,113 +161,74 @@ strike_prices = [S_0 * np.exp(N*(p*np.log(u) + (1-p)*np.log(d)))
                  for p in probabilities]
 
 
-# In[10]:
+# In[11]:
 
 
 # Bernoulli(p) = B(1, p)
 paths_1 = np.random.binomial(n=1, p=p_1, size=[L, N])
-E1_V_N_call = [np.mean(european_call(paths_1, K)) for K in strike_prices]
-E1_V_N_put = [np.mean(european_put(paths_1, K)) for K in strike_prices]
+E1_discounted_VN_call = [np.mean(european_call(paths_1, K) / (1+r)**N) for K in strike_prices]
+E1_discounted_VN_put = [np.mean(european_put(paths_1, K) / (1+r)**N) for K in strike_prices]
 
 paths_2 = np.random.binomial(n=1, p=p_2, size=[L, N])
-E2_V_N_call = [np.mean(european_call(paths_2, K)) for K in strike_prices]
-E2_V_N_put = [np.mean(european_put(paths_2, K)) for K in strike_prices]
+E2_discounted_VN_call = [np.mean(european_call(paths_2, K) / (1+r)**N) for K in strike_prices]
+E2_discounted_VN_put = [np.mean(european_put(paths_2, K) / (1+r)**N) for K in strike_prices]
 
 
 # ## Part (f)
 # 
 # By the martingale property, $V_0 = \tilde{E}(\frac{V_N}{(1+r)^N})$. But we compute $\tilde{E}$ using a Monte Carlo approach, so we need only average.
 
-# In[15]:
+# In[12]:
 
 
 paths_tilde = np.random.binomial(n=1, p=p_tilde, size=[L, N])
-Etilde_V_0_call = [np.mean(european_call(paths_tilde, K) / (1+r)**N) for K in strike_prices]
-Etilde_V_0_put = [np.mean(european_put(paths_tilde, K) / (1+r)**N) for K in strike_prices]
+Etilde_V0_call = [np.mean(european_call(paths_tilde, K) / (1+r)**N) for K in strike_prices]
+Etilde_V0_put = [np.mean(european_put(paths_tilde, K) / (1+r)**N) for K in strike_prices]
 
 
 # ## Part (g)
+# 
+# Modified in email. I still don't think I really understand this...
 
-# In[45]:
-
-
-path = np.random.binomial(n=1, p=1/2, size=N)
-
-delta_nm1, X_nm1  =     one_step_back(path, stock_value(path),
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm2, X_nm2 =     one_step_back(path[:-1], X_nm1,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm3, X_nm3 =     one_step_back(path[:-1], X_nm2,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-
-X_nm3
+# In[13]:
 
 
-# In[46]:
+for _ in range(3):
+    path = np.random.binomial(n=1, p=1/2, size=N-3)
+    extensions = np.array(list(product([0, 1], repeat=3)))
+    extended_paths = [np.append(path, extension) for extension in extensions]
 
-
-path = np.random.binomial(n=1, p=1/2, size=N)
-
-delta_nm1, X_nm1  =     one_step_back(path, stock_value(path),
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm2, X_nm2 =     one_step_back(path[:-1], X_nm1,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm3, X_nm3 =     one_step_back(path[:-1], X_nm2,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-
-X_nm3
-
-
-# In[47]:
-
-
-path = np.random.binomial(n=1, p=1/2, size=N)
-
-delta_nm1, X_nm1  =     one_step_back(path, stock_value(path),
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm2, X_nm2 =     one_step_back(path[:-1], X_nm1,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-delta_nm3, X_nm3 =     one_step_back(path[:-1], X_nm2,
-                  lambda x: european_call(x, strike_prices[0]).item(),
-                  stock_value)
-
-X_nm3
+    portfolio_from_path(path,
+                        lambda x: european_call(x, strike_prices[0]).item(),
+                        stock_value)
 
 
 # ## Part (h)
 
-# In[48]:
+# In[14]:
 
 
 results = np.vstack([
-    E1_V_N_call,
-    E1_V_N_put,
-    E2_V_N_call,
-    E2_V_N_put,
-    Etilde_V_0_call,
-    Etilde_V_0_put
+    E1_discounted_VN_call,
+    E1_discounted_VN_put,
+    E2_discounted_VN_call,
+    E2_discounted_VN_put,
+    Etilde_V0_call,
+    Etilde_V0_put
 ])
 
 names = [
-    'E1_V_N_call',
-    'E1_V_N_put',
-    'E2_V_N_call',
-    'E2_V_N_put',
-    'Etilde_V_0_call',
-    'Etilde_V_0_put'
+    'E1_discounted_VN_call',
+    'E1_discounted_VN_put',
+    'E2_discounted_VN_call',
+    'E2_discounted_VN_put',
+    'Etilde_V0_call',
+    'Etilde_V0_put'
 ]
 
-probabilities = ['p_tilde', 'p_1', 'p_2']
+strikes = ['K_tilde', 'K_1', 'K_2']
 
 pd.DataFrame(data=results,
              index=names,
-             columns=probabilities)
+             columns=strikes)
 
